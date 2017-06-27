634f67e93aa438100b2464420c2ac00298e0bbe2
Lea Medhurst <Lea Medhurst>
2017-06-27 16:58:37 +1000

    first release refactored

--- src/main/java/com/revelian/lea/App.java
+++ src/main/java/com/revelian/lea/App.java
@@ -8,16 +8,16 @@ package com.revelian.lea;
 public class App {
 	public static void main(String[] args) {
 
-	//	try {
+		try {
 			// Read the config file and find out how many iterations to run to
 			ReadConfig configVar = new ReadConfig();
 
 			// Run the algorithm
 			ApproxPi pi = new ApproxPi();			
 			System.out.println(pi.getPi(configVar.getNumberOfIterations()));
-		//} catch (Exception e) {
-		//	System.out.println("Couldn't find the value of pi , see below");
-		//	e.printStackTrace();
-	//	}
+		} catch (Exception e) {
+			System.out.println("Couldn't find the value of pi , see below");
+			e.printStackTrace();
+		}
 	}
 }
