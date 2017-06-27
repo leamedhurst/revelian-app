9c396e6d804131c698dd089f1f4993dd00603c3a
Lea Medhurst <Lea Medhurst>
2017-06-27 12:50:29 +1000

    First commit

---
+++ src/main/java/com/revelian/lea/App.java
@@ -0,0 +1,13 @@
+package com.revelian.lea;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
